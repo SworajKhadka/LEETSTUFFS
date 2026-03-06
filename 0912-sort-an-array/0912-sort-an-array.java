0c27002e3b4be84534d6f46e07dee0ace2a6e672
Sworaj Khadka <158992323+SworajKhadka@users.noreply.github.com>
2026-03-06 15:35:17 +0530

    Time: 26 ms (59.9%), Space: 86.5 MB (35.75%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,60 +1,41 @@
 class Solution {
 
     public int[] sortArray(int[] nums) {
-        mergeSort(nums, 0, nums.length - 1);
+        //we just have to call the function to do the work
+        divide(nums,0,nums.length-1);
         return nums;
+
     }
-
-    private void mergeSort(int[] nums, int left, int right) {
-
-        if (left >= right) return;
-
-        int mid = left + (right - left) / 2;
-
-        mergeSort(nums, left, mid);
-        mergeSort(nums, mid + 1, right);
-
-        merge(nums, left, mid, right);
+    private void divide(int[]nums, int left, int right){
+        if(left>=right) return;
+        int mid = left+(right-left)/2;
+        divide(nums,left,mid);
+        divide(nums,mid+1,right);
+        conquer(nums,left,mid,right);
     }
-
-    private void merge(int[] nums, int left, int mid, int right) {
-
-        int n1 = mid - left + 1;
-        int n2 = right - mid;
-
-        int[] L = new int[n1];
-        int[] R = new int[n2];
-
-        for (int i = 0; i < n1; i++)
-            L[i] = nums[left + i];
-
-        for (int j = 0; j < n2; j++)
-            R[j] = nums[mid + 1 + j];
-
-        int i = 0, j = 0;
-        int k = left;
-
-        while (i < n1 && j < n2) {
-            if (L[i] <= R[j]) {
-                nums[k] = L[i];
-                i++;
-            } else {
-                nums[k] = R[j];
-                j++;
+    private void conquer(int []nums,int left, int mid, int right){
+        int [] merge = new int[right-left+1];
+        int indx1=left;
+        int indx2=mid+1;
+        int x=0;
+        while(indx1 <= mid && indx2 <= right){
+            if(nums[indx1]<=nums[indx2]){
+                merge[x++]=nums[indx1++];
             }
-            k++;
-        }
+            else{
+                merge[x++]=nums[indx2++];
+            }
 
-        while (i < n1) {
-            nums[k] = L[i];
-            i++;
-            k++;
         }
-
-        while (j < n2) {
-            nums[k] = R[j];
-            j++;
-            k++;
+        while(indx1 <= mid){
+            merge[x++]=nums[indx1++];
+        }
+        while(indx2 <= right){
+            merge[x++]=nums[indx2++];
+        }
+        for(int i=0,j=left;i<merge.length;i++,j++){
+            nums[j]=merge[i];
         }
     }
+
 }
