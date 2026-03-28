198f800670c726f486eee35912f171372875ed97
Sworaj Khadka <158992323+SworajKhadka@users.noreply.github.com>
2026-03-28 23:50:42 +0530

    Time: 0 ms (100%), Space: 42.6 MB (94.98%) - LeetHub

--- 1317-convert-integer-to-the-sum-of-two-no-zero-integers/1317-convert-integer-to-the-sum-of-two-no-zero-integers.java
+++ 1317-convert-integer-to-the-sum-of-two-no-zero-integers/1317-convert-integer-to-the-sum-of-two-no-zero-integers.java
@@ -3,14 +3,14 @@ class Solution {
         for (int a = 1; a < n; a++) {
             int b = n - a;
             // Check if both 'a' and 'b' contain no zeros
-            if (isNoZero(a) && isNoZero(b)) {
+            if (isNotZero(a) && isNotZero(b)) {
                 return new int[]{a, b};
             }
         }
         return new int[]{}; // Should not reach here per constraints
     }
 
-    private boolean isNoZero(int num) {
+    private boolean isNotZero(int num) {
         while (num > 0) {
             // If the last digit is 0, it's not a 'No-Zero' integer
             if (num % 10 == 0) {
