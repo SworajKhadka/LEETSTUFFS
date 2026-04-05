a827a021d8d77a4549b63482422e81502f7e620d
Sworaj Khadka <158992323+SworajKhadka@users.noreply.github.com>
2026-04-05 18:25:59 +0530

    Time: 3 ms (98.85%), Space: 44 MB (19.8%) - LeetHub

--- 1408-string-matching-in-an-array/1408-string-matching-in-an-array.java
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.java
@@ -1,24 +1,19 @@
 class Solution {
     public List<String> stringMatching(String[] words) {
         List<String> ans = new ArrayList<>();
-        for(int i=0;i<words.length-1;i++){
-            for(int j=i+1;j<words.length;j++){
-                if(words[i].length()>words[j].length()){
-                    if(words[i].indexOf(words[j])!=-1){
-                        ans.add(words[j]);
-                    }
+        
+        // Sort words by length
+        Arrays.sort(words, (a, b) -> a.length() - b.length());
+        
+        for (int i = 0; i < words.length; i++) {
+            for (int j = i + 1; j < words.length; j++) {
+                if (words[j].contains(words[i])) {
+                    ans.add(words[i]);
+                    break; // avoid duplicates
                 }
-                else if(words[j].length()>words[i].length()){
-                    if(words[j].indexOf(words[i])!=-1){
-                        ans.add(words[i]);
-                    }
-                }
-                
-
             }
         }
-        HashSet<String> fans = new HashSet<>(ans);
-        return new ArrayList<>(fans);
-
+        
+        return ans;
     }
 }
